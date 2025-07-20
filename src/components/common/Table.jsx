c380ebe6cfe7d070fc2b47069f175433a9bbc942
officialmembypoudel <officialmembypoudel@gmail.com>
2025-07-20 15:07:57 +0545

    -- transactions display

--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 
-const Table = () => {
+const Table = ({ data = [], columns = [] }) => {
   return (
     <table className="w-full">
       <thead>
         <tr className="border-b border-gray-300 hover:bg-gray-50">
-          <th className="py-4 p-2 text-left text-gray-500 text-sm font-medium">
-            Phone
-          </th>
-          <th className="py-4 p-2 text-left text-gray-500 text-sm font-medium">
-            Name
-          </th>
-          <th className="py-4 p-2 text-left text-gray-500 text-sm font-medium">
-            City
-          </th>
+          {columns.map((col) => {
+            return (
+              <th className="py-4 p-2 text-left text-gray-500 text-sm font-medium">
+                {col.label}
+              </th>
+            );
+          })}
         </tr>
       </thead>
       <tbody>
-        <tr className="border-b border-gray-300  hover:bg-gray-50">
-          <td className="py-4 p-2 text-sm font-medium">9876543455</td>
-          <td className="py-4 p-2 text-sm">Ram</td>
-          <td className="py-4 p-2 text-sm">Janakpur</td>
-        </tr>
-        <tr className="border-b border-gray-300  hover:bg-gray-50">
-          <td className="py-4 p-2 text-sm font-medium">9876543458</td>
-          <td className="py-4 p-2 text-sm">Mars</td>
-          <td className="py-4 p-2 text-sm">Sydney</td>
-        </tr>
-        <tr className=" hover:bg-gray-50">
-          <td className="py-4 p-2 text-sm font-medium">9876543459</td>
-          <td className="py-4 p-2 text-sm">Pluto</td>
-          <td className="py-4 p-2 text-sm">New York</td>
-        </tr>
+        {data?.map((row) => {
+          return (
+            <tr className="border-b border-gray-300  hover:bg-gray-50">
+              {columns.map((col) => {
+                return (
+                  <td className="py-4 p-2 text-sm font-medium">
+                    {col.renderDetail ? col.renderDetail(row) : row[col.key]}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,17 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "../components/common/Table";
 import Card from "../components/common/Card";
 
-const Transactions = () => {
-  return (
-    <div className="px-2">
-      <h1 className="text-5xl mb-8">Transactions</h1>
+const columns = [
+  {
+    label: "Book",
+    key: "book",
+    renderDetail: (row) => {
+      return row?.book?.title;
+    },
+  },
+  {
+    label: "Issued To",
+    key: "issuedTo",
+    renderDetail: (row) => {
+      return row?.issuedTo?.name;
+    },
+  },
+  {
+    label: "Issued By",
+    key: "issuedBy",
+    renderDetail: (row) => {
+      return row?.issuedBy?.name;
+    },
+  },
+  {
+    label: "Returned",
+    key: "returned",
+    renderDetail: (row) => {
+      return row.returned ? "Yes" : "No";
+    },
+  },
+  {
+    label: "Issue Date",
+    key: "issueDate",
+    renderDetail: (row) => {
+      const date = row.issueDate;
+      return new Date(date).toDateString();
+    },
+  },
+];
 
-      <Card customClass="bg-white border border-gray-300">
-        <h4 className="text-2xl mb-4 font-bold">Transaction History</h4>
-        <Table />
-      </Card>
-    </div>
+const Transactions = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  const fetchTransactions = async () => {
+    try {
+      const response = await fetch("http://localhost:5003/api/transactions", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const responseData = await response.json();
+
+      console.log(responseData);
+
+      if (responseData.success) {
+        setTransactions(responseData.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  return (
+    <>
+      <div className="p-4 px-8  mb-8 shadow">
+        <h4 className="text-3xl font-semibold">Transactions</h4>
+      </div>
+      <div className="px-8">
+        <Card customClass="bg-white border border-gray-300">
+          <h5 className="text-2xl mb-4 font-bold">Transaction History</h5>
+          <Table columns={columns} data={transactions} />
+        </Card>
+      </div>
+    </>
   );
 };
 
